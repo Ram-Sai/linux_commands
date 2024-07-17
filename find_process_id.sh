ab6e8521834553ccdd3a8fd1d9e0f4ba49a23dae
Ram-Sai <ram.sep17@gmail.com>
2024-07-17 06:37:02 +0000

    updated the comments in find_process_id.sh file

--- find_process_id.sh
+++ find_process_id.sh
@@ -4,3 +4,10 @@
 #
 #
 ps -ef | grep amazon | awk -F" " '{print $2}'
+
+# in the above command ps -ef will list all the processes running in the computer with details like process id and process name etc.
+# grep command is used to search for specific keyword
+# awk -f" " '{print $2}' is used to print the second column value
+# | is used to pipe the data from first command to second command
+# 
+# in the above example we are searching for all the processes using ps -ef the data that is returned is again piped to the next command grep amazon that will list the entries which has amazon in it  and that again is piped to just display the value in the second column 
