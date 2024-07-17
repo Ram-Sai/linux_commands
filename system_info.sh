f9d141e536fb6b7662d0fa2bb7d07c40f4f94790
Ram-Sai <58572123+Ram-Sai@users.noreply.github.com>
2024-07-17 12:42:57 +0530

    Update system_info.sh

--- system_info.sh
+++ system_info.sh
@@ -5,7 +5,7 @@
 #
 set -x  # this will set the file for debug mode so that it also displays the commands along with the result 
 set -e  # this will exit the script when there is any error
-set -o  # this will work in combination with set -e command. usually set -e just looks for the error on the last command even when there is pipe. but when you add set -o this will check for the error for all commands and not just last command in the piped command.
+set -o  pipefail # this will work in combination with set -e command. usually set -e just looks for the error on the last command even when there is pipe. but when you add set -o this will check for the error for all commands and not just last command in the piped command.
 echo "Storage"
 
 df -h # this will display the information about the storage in GB's
